01310df0efa08dad2dbdd34998e975ce155b1a7c
Tiffany <tiffanyjdow@gmail.com>
2018-06-07 19:24:22 +0000

    used sort to change array

---
+++ students.js
@@ -0,0 +1,25 @@
+//sorts first by name ascending alphabetically
+//if names are equal, sort by descending age
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+students.sort(function(a, b){
+
+  if (a.name < b.name) {
+    return -1;
+  }
+  else if (a.name > b.name) {
+    return 1;
+  }
+  else {
+    if (a.age > b.age) {
+      return -1;
+    } else if (a.age < b.age) {
+      return 1;
+    }
+  }
+});
